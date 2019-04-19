34014d55b1f57581462a42743a9491bbc36a4069
Nazar <ustymchyk.nazar@gmail.com>
2019-04-19 18:04:45 +0300

    js time fix

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   function setDate() {
     const date = new Date();
     const sec = (date.getSeconds() / 60) * 360;
-    const min = ((date.getMinutes() / 60) * 360) ;
-    const hour = ((date.getHours() / 12) * 360);
+    const min = ((date.getMinutes() / 60) * 360) + sec / 60;
+    const hour = ((date.getHours() / 12) * 360) + min / 12;
 
     watch.style.setProperty('--sec', `${sec}deg`);
     watch.style.setProperty('--min', `${min}deg`);
